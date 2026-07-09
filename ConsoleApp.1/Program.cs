2b68e46fdbc95e45d4bd9f965cb1844df56e75fd
ujjwal verma <ujjwalverma7015@gmail.com>
2026-07-09 13:27:46 +0530

    Add project files.

---
+++ ConsoleApp.1/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.WriteLine("Hello, World!");
+/*int a = 20;
+int b = 10;
+int c = 40;
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("a is big");
+    }
+    else
+    {
+        Console.WriteLine("c is big ");
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine("b is big ");
+    }
+    else
+    {
+        Console.WriteLine("c is big ");
+    }
+}*/
+
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 40;
+if (a > b)
+{
+    if (a > c)
+    {
+        if (a > d)
+        {
+            Console.WriteLine("a is big");
+        }
+        else
+        {
+            Console.WriteLine("d is big ");
+        }
+    }
+    else
+    {
+        if (c > d)
+        {
+            Console.WriteLine("c is big");
+        }
+        else
+        {
+            Console.WriteLine("d is big");
+        }
+    }
+
+}
+else
+{
+    if (b > c)
+    {
+        if (b > d)
+        {
+            Console.WriteLine("b is big ");
+        }
+        else
+        {
+            Console.WriteLine("d is big ");
+        }
+    }
+    else
+    {
+        if (c > d)
+        {
+            Console.WriteLine("c is big ");
+        }
+        else
+        {
+            Console.WriteLine("d is big");
+        }
+    }
+}
+
+
+Console.WriteLine("ujjwal");
+
+
+
+
+
